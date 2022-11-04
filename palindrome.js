2393a2f20b99bc6a43b58d478e4a29afb23b9f29
Suman Kumar Sharma <sks143info@gmail.com>
2022-11-04 22:02:48 +0530

    created prime and palindrome code

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+function palindrome(str){
+let bag="";
+for(let i=str.length-1; i>=0; i++){
+bag+=str[i];
+}
+if(bag==str]{
+console.log("palindrome");
+}
+else
+{
+console.log("Not palindrome");
+}
+}
+str(naman);
---
+++ prime.js
@@ -0,0 +1,10 @@
+function checkPrimen(num){
+factore =0;
+for(let i=0; i<num.length; i++){
+if(num%2==1)
+}
+if(factore==2){
+console.log("prime")
+}else{
+console.log("Not Prime");
+}
