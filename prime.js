4740bb1a6d54297731b2b32e35e0b83a44c82136
Suman Kumar Sharma <sks143info@gmail.com>
2022-11-04 22:13:57 +0530

    prime code are fixed

--- prime.js
+++ prime.js
@@ -1,10 +1,12 @@
 function checkPrimen(num){
 factore =0;
 for(let i=0; i<num.length; i++){
-if(num%2==1)
+if(num%2==1){
+factor++;
 }
 if(factore==2){
 console.log("prime")
 }else{
 console.log("Not Prime");
 }
+checkPrime(11);
